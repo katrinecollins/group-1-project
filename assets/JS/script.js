710c049a5708c302cf7ae13afbabf269faeca7c2
Vashti <patrena@sbcglobal.net>
2021-07-10 13:39:38 -0700

    Updated Javascript,new second API, new design layout, and population of website

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,6 +1,13 @@
+var historySearch =[]
+if(localStorage.getItem("Movie History")){
+    historySearch=JSON.parse(localStorage.getItem("Movie History"))
+}
+
 function searchMovie(){
     var searchText=document.querySelector("#searchText").value    
-    
+    historySearch.push(searchText)
+    localStorage.setItem("Movie History", JSON.stringify(historySearch));
+
 fetch(`http://www.omdbapi.com/?apikey=803bb7b9&s=${searchText}`)
 .then(function(response){
     console.log("1st response",response)
@@ -10,61 +17,43 @@ fetch(`http://www.omdbapi.com/?apikey=803bb7b9&s=${searchText}`)
 .then(function(response){
     console.log("OMDB response",response);
     provideData(response);
-    var Search = response.Search[0].Title
-    return fetch(`https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch=${Search}`)
+    console.log(searchText);
+    // var Search = response.Search[0].Title
+    return fetch(`https://api.themoviedb.org/3/search/movie?api_key=e03d31621bcab7d9194f6a08d1399d8f&language=en-US&query=${searchText}`)
 })
-
-.then(function(wikiResponse) {
-    return wikiResponse.json();
+.then(function(themoviedbResponse) {
+    return themoviedbResponse.json();
 })
 
 .then(function(response){
-    console.log("Wikipedia Response",response);
+    console.log("themoviedb Response",response);
+    generateData(response);
+    console.log(searchText);
 })
-
-}
-
-// fetch(`http://www.omdbapi.com/?apikey=803bb7b9&s=flash`)
-// .then(function(response){
-//     console.log("1st response",response)
-//     return response.json();
-// })
-// // .then(function(response){
-// //     console.log("2nd response",response);
-// //     provideData(response);
-// // })
-
-// .then(function(response){
-//     console.log("OMDB response",response);
-//     provideData(response);
-//     var Search = response.Search[0].Title
-//     return fetch(`https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch=${Search}`)
-// })
-
-// .then(function(wikiResponse) {
-//     return wikiResponse.json();
-// })
-
-// .then(function(response){
-//     console.log("Wikipedia Response",response);
-// })
 
 function provideData(data){
     console.log("this is the data to provide data function",data.Search[0]);
     document.querySelector("#Title").textContent=data.Search[0].Title;
-    // document.querySelector("#Title").textContent=search
     document.querySelector("#Poster").src=data.Search[0].Poster;
-    // document.querySelector("#Poster").textContent=search
-    document.querySelector("#Year").textContent=data.Search[0].Year;
-    // document.querySelector("#Year").textContent=search
+    document.querySelector("#Year").textContent=data.Search[0].Year; 
 }
-var responseContainer=document.querySelector("#response-container")
+function generateData(data){
+    console.log("This is the data from generate data function",data.results[0]);
+    console.log("This is the data from generate data function",data.results[0].poster_path);
+    document.querySelector("#overview").textContent=data.results[0].overview;
+    document.querySelector("#release_date").textContent=data.results[0].release_date;
+    document.querySelector("#popularity").textContent=data.results[0].popularity;
+    document.querySelector("#vote_count").textContent=data.results[0].vote_count;
+    document.querySelector("#poster_path").src=`https://image.tmdb.org/t/p/w500${data.results[0].poster_path}`;
+    document.querySelector("#original_title").textContent=data.results[0].original_title;
+}
+// var responseContainer=document.querySelector("#response-container")
 
-var text = document.createElement("text");
-//text.setAttribute('src', response.data[0]
-var TitleNameArr =[""];
-var ls=JSON.parse(localStorage.getItem("Title-list"));
-var movieBtns=document.querySelector("#Title-Btns");
+// var text = document.createElement("text");
+// //text.setAttribute('src', response.data[0]
+// var TitleNameArr =[""];
+// var ls=JSON.parse(localStorage.getItem("Title-list"));
+// var movieBtns=document.querySelector("#Title-Btns");
 
 // if(ls) {console.log("there is no localstorage");
 // }else {
@@ -77,4 +66,4 @@ var movieBtns=document.querySelector("#Title-Btns");
 // var movieEl =document.querySelector("movie-name");
 // var movie=movieEl.value;
 // console.log("movie is:", movie);
-// }
+}
